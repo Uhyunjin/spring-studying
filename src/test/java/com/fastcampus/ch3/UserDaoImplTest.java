f0b07266ca6896aacfb61a2637214c58e5ff71df
uhyunjin <uhyeonjin0619@gmail.com>
2024-03-13 23:12:48 +0900

    user date형식 바꿔주고 테스트 재시도

--- src/test/java/com/fastcampus/ch3/UserDaoImplTest.java
+++ src/test/java/com/fastcampus/ch3/UserDaoImplTest.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.util.Calendar;
 import java.util.Date;
 
 import static org.junit.Assert.*;
@@ -30,9 +31,31 @@ public class UserDaoImplTest {
 
     @Test
     public void updateUser() {
-        User user = new User("asdf", "1234", "abc", "aaa@aaa.com", new Date(), "Fb", new Date());
+
+        Calendar cal = Calendar.getInstance();
+        cal.clear(); //처음에 초기화 시켜주기
+        cal.set(2021, 1, 1);
+        //날짜에 넣어 줄 캘린더 객체 생성
+
+        userDao.deleteUser("asdf");
+
+        User user = new User("asdf", "1234", "abc", "aaa@aaa.com", new Date(cal.getTimeInMillis()), "Fb", new Date());
+        // 뒤 시간을 자르고 날짜만 넣기
+        // sql date형식은 날짜만인데 자바의 date는 시간까지 들어가서 date로 생성한 user와 sql에서 가져온 user2가 같지 않았던 것
         int rowCnt = userDao.insertUser(user);
         assertTrue(rowCnt==1);
+
+        user.setPwd("4321");
+        user.setEmail("bbb@bbb.com");
+        rowCnt = userDao.updateUser(user);
+        assertTrue(rowCnt ==1);
+
+        User user2 = userDao.selectUser(user.getId());
+        System.out.println("user = " + user);
+        System.out.println("user2 = " + user2);
+        assertTrue(user.equals(user2));
+        //시간 정보 삭제 후 통과
+
 
     }
 }
